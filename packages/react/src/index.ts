d23a1f72b93707a20203e3a67923e87cd3220e77
Kris Renaldi <krenaldi@comcast.net>
2024-09-17 08:50:14 -0400

    Downgraded yarn packages in order for es;int and prettier to work with workspaces

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,11 @@
 const Hello = (name?: string) => `${name || "World"}`;
 
+const user = {
+  jobTitle: 'Software Engineer',
+};
+
+if ((user.jobTitle = 'Junior Enginner')) {
+  user.jobTitle = 'Bug Introducer';
+}
+
 export default Hello;
